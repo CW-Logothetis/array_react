feb73b44baabb044348cd34549c6944b884f393e
CW-Logothetis <chris.logothetis@cognitivplus.com>
2023-12-09 20:32:08 +0200

    improve style of ArrayMethodPage.jsx

--- src/utils/styleHeadings.js
+++ src/utils/styleHeadings.js
@@ -3,5 +3,19 @@ export function styleHeadings(output) {
     const explanationOutput = output.replace("EXPLANATION", "<div style='margin-bottom: 0.5rem; font-weight: 600'>EXPLANATION</div>");
     const syntaxOutput = explanationOutput.replace("SYNTAX", "<div style='margin: 20px 0 0.5rem; font-weight: 600'>SYNTAX</div>")
     newOutput = syntaxOutput.replace("EXAMPLES", "<div style='margin: 20px 0 0.5rem; font-weight: 600'>EXAMPLES</div>")
-    return newOutput;
+    return formatCodeBlocks(newOutput);
 }
+
+function formatCodeBlocks(text) {
+    // Replace triple backticks with <pre><code>...</code></pre>
+    text = text.replace(/```([\s\S]+?)```/g, '<pre>$1</pre>');
+
+    // Replace single backticks with <code>...</code>
+    text = text.replace(/`([^`]+)`/g, '<code>$1</code>');
+
+    text = text.replace(/(```\s*)javascript/ig, '$1');
+
+    return text;
+}
+
+
