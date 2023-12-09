f8979246e9812d2c0f0fde2ca61f0de1d150884a
CW-Logothetis <chris.logothetis@cognitivplus.com>
2023-12-09 20:31:50 +0200

    css

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Layout.css';
 
 function Layout({ children }) {
     return (
-        <>
+        <div className="layout">
             <nav>
                 <Link to="/">Home</Link>
             </nav>
             <main>{children}</main>
-        </>
+        </div>
     );
 }
 
