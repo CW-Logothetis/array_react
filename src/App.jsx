d203d8d0d66bb7c4ea62111901a8e89d842c992b
CW-Logothetis <chris.logothetis@cognitivplus.com>
2024-05-08 12:12:47 +0300

    move old pages into page folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import HomePage from './components/HomePage';
-import ArrayMethodPage from './components/ArrayMethodPage';
+import HomePage from './pages/HomePage.jsx';
+import ArrayPage from './pages/ArrayPage.jsx';
+import ArrayMethodPage from './pages/ArrayMethodPage';
 import './App.css';
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<HomePage/>}/>
+            <Route path="/arrays" element={<ArrayPage/>}/>
             <Route path="/arrays/:array_method" element={<ArrayMethodPage/>}/>
         </Routes>
     );
