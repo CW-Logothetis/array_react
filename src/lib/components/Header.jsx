1dbc05ea2831fd0bfb47949c4982df3f983b7f19
CW-Logothetis <chris.logothetis@cognitivplus.com>
2024-05-28 11:22:25 +0300

    sign in out btn header

--- src/lib/components/Header.jsx
+++ src/lib/components/Header.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import {Link, NavLink} from 'react-router-dom';
-import logo from '../../assets/images/logo_methodMinder.png'
-
-import './layout/layout.css'
+import React, { useContext } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { supabase } from '../../services/supabaseClient';
+import { AuthContext } from '../../App';
+import logo from '../../assets/images/logo_methodMinder.png';
+import './layout/layout.css';
 
 export default function Header() {
+    const { user } = useContext(AuthContext);
+
+    const handleSignOut = async () => {
+        await supabase.auth.signOut();
+    };
 
     return (
         <header>
             <div className="header-links">
-                <Link className="site-logo" to="/"> <img src={logo} alt="Site logo"/></Link>
+                <Link className="site-logo" to="/"> <img src={logo} alt="Site logo" /></Link>
                 <nav>
-                    <NavLink
-                        to="/"
-                        className={({isActive}) => isActive ? "active-link" : null}
-                    >
-                        Study
-                    <
-                /NavLink>
-                    <NavLink
-                        to="/arrays"
-                        className={({isActive}) => isActive ? "active-link" : null}
-                    >
-                        Arrays
-                    </NavLink>
+                    <NavLink to="/" className={({ isActive }) => isActive ? "active-link" : null}>Study</NavLink>
+                    <NavLink to="/arrays" className={({ isActive }) => isActive ? "active-link" : null}>Arrays</NavLink>
+                    {user ? (
+                        <button onClick={handleSignOut} className='button text-only'>Sign Out</button>
+                    ) : (
+                        <NavLink to="/auth" className={({ isActive }) => isActive ? "active-link" : null}>Sign In</NavLink>
+                    )}
                 </nav>
             </div>
         </header>
     );
-};
+}
+
